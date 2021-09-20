3cdb2a933e487550285f5ce0d2d9c4de4d76a4e6
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-09-20 18:16:13 -0400

    Ported over the main class and the rest of the config
    now it like the original

--- src/main/java/panda/birdsnests/Config.java
+++ src/main/java/panda/birdsnests/Config.java
@@ -1,12 +1,13 @@
 package panda.birdsnests;
 
-
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
+
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,41 +18,27 @@ import java.nio.file.Path;
 public class Config
 {
     private static final Logger LOGGER = LogManager.getLogger();
-
-
-    // Builder
     private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
     private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
-
     public static ForgeConfigSpec COMMON_CONFIG;
     public static ForgeConfigSpec CLIENT_CONFIG;
-
-
-
-    // Categories
     public static final String CATEGORY_GENERAL = "general";
-
-
-    // Config setting
-    public static ForgeConfigSpec.BooleanValue NEST_ALLOW_STACKING;
-    public static ForgeConfigSpec.BooleanValue NEST_ALLOW_DECAY_DROPS;
-    public static ForgeConfigSpec.IntValue NEST_RARITY;
-    public static ForgeConfigSpec.DoubleValue NEST_DROP_MODIFIER;
+    public static ForgeConfigSpec.BooleanValue allowStacking;
+    public static ForgeConfigSpec.BooleanValue allowDecayDrops;
+    public static ForgeConfigSpec.IntValue nestRarity;
+    public static ForgeConfigSpec.DoubleValue decayDropModifier;
 
 
 
 
 
-    // Set the settings
     static {
 
         COMMON_BUILDER.comment("Bird Nest Settings").push(CATEGORY_GENERAL);
-
-        NEST_ALLOW_STACKING = COMMON_BUILDER.comment("ALLOW_STACKING").define("nestAllowStacking", false);
-        NEST_RARITY = COMMON_BUILDER.comment("NEST_DROP_RARITY").defineInRange("nestRarityNormal", 40, 0, 1000);
-        NEST_ALLOW_DECAY_DROPS = COMMON_BUILDER.comment("ALLOW_DECAY_DROPS").define("nestAllowDecayDrops", true);
-        NEST_DROP_MODIFIER = COMMON_BUILDER.comment("NEST_DECAY_DROP_MULTIPLIER").defineInRange("nestRarityNormal", 1.25F, 0, 1000);
-
+        nestRarity = COMMON_BUILDER.comment("[range: 0 ~ 1000, default: 40]").defineInRange("NEST_DROP_RARITY", 40, 0, 1000);
+        allowStacking = COMMON_BUILDER.comment("Allows to enable/disable nests stacking [default: false]").define("ALLOW_STACKING", false);
+        decayDropModifier = COMMON_BUILDER.comment("This makes nests more (or less ) rare from decaying leaves. Leave at 1 for no change. [range: 0.0 ~ 1000.0, default: 1.25]").defineInRange("NEST_DECAY_DROP_MULTIPLIER", 1.25F, 0, 1000);
+        allowDecayDrops = COMMON_BUILDER.comment("Allows to enable/disable nests dropping from decaying leaves [default: true]").define("ALLOW_DECAY_DROPS", true);
         COMMON_BUILDER.pop();
 
         COMMON_CONFIG = COMMON_BUILDER.build();
