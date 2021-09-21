f80dd3adbb6d6ab4645b90209e8a6c8398e04f4a
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-09-21 00:14:01 -0400

    Ported the Bird Nest
    to 1.14.4

--- src/main/java/panda/birdsnests/ItemNest.java
+++ src/main/java/panda/birdsnests/ItemNest.java
@@ -1,81 +1,69 @@
 package panda.birdsnests;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;//ADD NEW STAT
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.loot.*;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
-public class ItemNest extends Item {
+import java.util.List;
 
+public class ItemNest extends Item
+{
 	private static final ResourceLocation LOOT_TABLE = new ResourceLocation("birdsnests:nest_loot");
 
-	public ItemNest(){
-		this.setCreativeTab(CreativeTabs.MISC);
-	}
 
-	@Override
-	public String getUnlocalizedName(ItemStack stack) {
-		return super.getUnlocalizedName() + "." + (stack.getItemDamage() == 0 ? "default" : "32x");
-	}
-
-	@Override
-	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+	public ItemNest(String name)
 	{
-		if (this.isInCreativeTab(tab))
-		{
-			items.add(new ItemStack(this, 1, (BirdsNests.use32) ? 1 : 0));
-		}
+		super(new Item.Properties()
+				.maxStackSize(BirdsNests.nestStackSize)
+				.group(ItemGroup.MISC)
+		);
+		this.setRegistryName(new ResourceLocation(BirdsNests.MODID, name));
 	}
 
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand)
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn)
 	{
-		ItemStack itemstack = playerIn.getHeldItem(hand);
-
-		if (!playerIn.capabilities.isCreativeMode)
-		{
-			itemstack.shrink(1);
-		}
-
-		worldIn.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		ItemStack itemstack = playerIn.getHeldItem(handIn);
+		itemstack.shrink(1);
+		worldIn.playSound((PlayerEntity)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
 
 		if (!worldIn.isRemote)
 		{
-			
-			handleRandomItems(worldIn,playerIn);
+			this.generateLoot(worldIn,playerIn);
 		}
-		return new ActionResult(EnumActionResult.PASS, itemstack);
+
+		return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
 	}
 
 
-	private void handleRandomItems(World world,EntityPlayer player) {
 
-		LootTable loottable = world.getLootTableManager().getLootTableFromLocation(LOOT_TABLE);
-		
-		LootContext.Builder builder = new LootContext.Builder((WorldServer)world);
-		 for (ItemStack itemstack : loottable.generateLootForPools(world.rand, builder.build()))
+
+
+	private void generateLoot(World world,PlayerEntity player)
+	{
+		if (LOOT_TABLE == LootTables.EMPTY)
 		{
-			
-			EntityItem entityitem = new EntityItem(world, player.posX, player.posY + 1.5D, player.posZ, itemstack);
-			entityitem.motionX = world.rand.nextGaussian() * 0.05F;
-			entityitem.motionY = (0.2d);
-			entityitem.motionZ = world.rand.nextGaussian() * 0.05F;
-			//player.getHorizontalFacing()
+		}
+		else
+		{
+			LootTable loottable = ServerLifecycleHooks.getCurrentServer().getLootTableManager().getLootTableFromLocation(LOOT_TABLE);
+			LootContext.Builder builder = new LootContext.Builder((ServerWorld) world);
+			LootContext lootcontext = builder.withParameter(LootParameters.POSITION, player.getPosition()).withParameter(LootParameters.THIS_ENTITY, player).build(LootParameterSets.GIFT);
 
-			world.spawnEntity(entityitem);
+			List<ItemStack> itemstacklist = loottable.generate(lootcontext);
+
+			for (ItemStack itemstack : itemstacklist)
+			{
+				ItemEntity entityitem = new ItemEntity(world, player.posX, player.posY + 1.5D, player.posZ, itemstack);
+				world.addEntity(entityitem);
+			}
 		}
 	}
 }
