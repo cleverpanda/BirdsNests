9d1ccdc9c9f6b1125138a6a22b46b011ebde21f6
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2021-11-16 22:13:11 -0500

    1.14.4 (#20)
    * Initial set up
    
    for 1.14.4
    
    * Ported over mcmod.info
    
    now is mods.toml
    
    * ported over the config
    
    had to be in it's own class
    
    * Ported over the main class and the rest of the config
    
    now it like the original
    
    * some orginization
    
    more organized
    
    * Ported over the DecayLeafEvent
    
    to 1.14.4
    
    * Ported the Bird Nest
    
    to 1.14.4
    
    * ported Bird nest model
    
    had to change the name of it
    
    * ported over the nest loot table
    
    got rid of the back up loot table and 32 bit model
    
    * Ported over the registry system
    
    Had to be in it's own class
    
    * ported over all the language files
    
    all language files are up to date now
    
    * Finished porting
    
    to 1.14.4
    
    * fixed translations
    
    accidently put bird nests instead of next XD

---
+++ src/main/java/panda/birdsnests/Config.java
@@ -0,0 +1,63 @@
+package panda.birdsnests;
+
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.file.Path;
+
+@Mod.EventBusSubscriber
+public class Config
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
+    private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
+    public static ForgeConfigSpec COMMON_CONFIG;
+    public static ForgeConfigSpec CLIENT_CONFIG;
+    public static final String CATEGORY_GENERAL = "general";
+    public static ForgeConfigSpec.BooleanValue allowStacking;
+    public static ForgeConfigSpec.BooleanValue allowDecayDrops;
+    public static ForgeConfigSpec.IntValue nestRarity;
+    public static ForgeConfigSpec.IntValue decayDropModifier;
+
+    static
+    {
+        COMMON_BUILDER.comment("Bird Nest Settings").push(CATEGORY_GENERAL);
+        nestRarity = COMMON_BUILDER.comment("[range: 0 ~ 1000, default: 40]").defineInRange("NEST_DROP_RARITY", 40, 0, 1000);
+        allowStacking = COMMON_BUILDER.comment("Allows to enable/disable nests stacking [default: false]").define("ALLOW_STACKING", false);
+        decayDropModifier = COMMON_BUILDER.comment("This makes nests more (or less ) rare from decaying leaves. Leave at 1 for no change. [range: 0.0 ~ 1000.0, default: 1.25]").defineInRange("NEST_DECAY_DROP_MULTIPLIER", 1, 0, 1000);
+        allowDecayDrops = COMMON_BUILDER.comment("Allows to enable/disable nests dropping from decaying leaves [default: true]").define("ALLOW_DECAY_DROPS", true);
+        COMMON_BUILDER.pop();
+
+        COMMON_CONFIG = COMMON_BUILDER.build();
+        CLIENT_CONFIG = CLIENT_BUILDER.build();
+    }
+
+    public static void loadConfig(ForgeConfigSpec spec, Path path)
+    {
+        final CommentedFileConfig configData = CommentedFileConfig.builder(path)
+                .sync()
+                .autosave()
+                .writingMode(WritingMode.REPLACE)
+                .build();
+        configData.load();
+        spec.setConfig(configData);
+    }
+
+    @SubscribeEvent
+    public static void onLoad(final ModConfig.Loading configEvent)
+    {
+        LOGGER.debug("Config Loaded Event ");
+    }
+
+    @SubscribeEvent
+    public static void onReload(final ModConfig.ConfigReloading configEvent) { LOGGER.debug("Config Re-Loaded Event "); }
+
+}
